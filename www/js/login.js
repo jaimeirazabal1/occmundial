8e7e3794083e92c87018713a0465bba14d6ff022
tayme <taymetayme8@gmail.com>
2016-07-20 20:48:59 -0430

    first commit

---
+++ www/js/login.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+   $("#login").click(function(event) {
+   		event.preventDefault();
+   		var usuario = $("#usuario").val();
+   		var contrasena = $("#contrasena").val();
+   		//contrasena = $.md5(contrasena);
+   	
+   		$.ajax({
+			    // En data puedes utilizar un objeto JSON, un array o un query string
+			    data: {usuario : usuario, contrasena : contrasena, controlador: "login"},
+			    //Cambiar a type: POST si necesario
+			    type: "POST",
+			    // Formato de datos que se espera en la respuesta
+			    dataType: "json",
+			    // URL a la que se enviará la solicitud Ajax
+			    url: "http://tayme.esy.es/aplicaciones/peticiones/login.php",
+			    //url: "192.168.0.103"
+			})
+			 .done(function( data, textStatus, jqXHR ) {
+			         
+			     if ( console && console.log && data) {
+			         console.log( "La solicitud se ha completado correctamente." );
+			         window.sessionStorage.setItem("usuario",data.usuario);
+			     	 window.sessionStorage.setItem("tipo",data.tipo);			     
+			     	 window.sessionStorage.setItem("id_usuario",data.id_usuario);
+			     	 window.location="bienvenida.html";
+			     }else{
+			     	$(".usuario_clave_error").show("slow");
+			         
+			         setTimeout(function(){ 
+			         	$(".usuario_clave_error").hide("slow");
+			          }, 5000);
+			     }
+
+
+			 })
+			 .fail(function( jqXHR, textStatus, errorThrown ) {
+			     if ( console && console.log ) {
+			         console.log( "La solicitud a fallado: " +  textStatus);
+			         $(".usuario_clave_error").show("slow");
+			         
+			         setTimeout(function(){ 
+			         	$(".usuario_clave_error").hide("slow");
+			          }, 5000);
+
+
+			     }
+			});
+   });
+});
